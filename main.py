ae6283fddf81afbf8159d0f86a31768adf4626a8
BubansBrahian <bubansbrahian@gmail.com>
2026-04-15 14:33:50 -0300

    agrego comentarios en el codigo referidos a la logica de funcionamiento

--- main.py
+++ main.py
@@ -12,35 +12,34 @@ d = dht.DHT11(machine.Pin(15))
 
 #r es el rele
 r=machine.Pin(16, machine.Pin.OUT)
-r.value(1)
+r.value(1) #rele activo en bajo
 
 #led es el led del la rasperry
 led=machine.Pin("LED", machine.Pin.OUT)
 
+#se encuentra el ID del dispositivo
 ID_del_dispositivo = binascii.hexlify(machine.unique_id()).decode()
 archivo_config="configuracion.json"
 
-
+#Cargo la configuracion  o establce por defecto si no hay archivo
 def cargar_config():
     try:
         with open(archivo_config, "r") as f:
             return json.load(f)
     except OSError:
         return {
-            "setpoint": 24.0,
+            "setpoint": 25.0,
             "periodo": 5,
-            "modo": "auto",
-            "rele": 0
+            "modo": "auto",  #Se utiliza "auto" no tener que escribir automatico 
+            "rele": 1
         }
-
+    
+#guarda la modificaciones en el archivo json
 def guardar_config(ajustes_datos):
     with open(archivo_config, "w") as archivo:
         json.dump(ajustes_datos, archivo)
 
 
-async def wifi_han(state):
-    print('Wifi is ', 'up' if state else 'down')
-    await asyncio.sleep(1) 
 
 async def conn_han(client):
     await client.subscribe(f"{ID_del_dispositivo}/setpoint", 1)
@@ -54,6 +53,7 @@ evento_destello = asyncio.Event()
 
 ajustes = cargar_config()
 
+# procesa los mensajes recibidos y actualiza json si es que hubo modificaciones
 async def procesar_mensajes(client):
     async for topic, msg, retained in client.queue:
         
@@ -76,7 +76,7 @@ async def procesar_mensajes(client):
                 else:
                      ban=False
             if "rele" in t:
-                ajustes["rele"] = int(m)
+                ajustes["rele"] = int(m)   #si se pone un 1 se apaga el rele y si se pone un 0 se enciende el rele es activo bajo
                 ban=True
             if "destello" in t:
                 evento_destello.set() 
@@ -85,6 +85,7 @@ async def procesar_mensajes(client):
             if ban == True:
                 guardar_config(ajustes)
 
+#logica del bucle de control para el caso de rele manual y la temperatura para el caso auto
 async def bucle_control(client):
     while True:
         try:
@@ -126,9 +127,7 @@ async def tarea_destello():
         evento_destello.clear()
 
 
-# Configurar parámetros adicionales de mqtt_as
-config['subs_cb'] = lambda *args: None #  para evitar error si no se usa callback
-config['wifi_coro'] = wifi_han
+#configuracion de MQTT y del cliente
 config['queue_len'] = 10
 config['ssl'] = True
 MQTTClient.DEBUG = True  
@@ -142,18 +141,18 @@ async def main():
 
     await conn_han(client)
     
+    # se ejecutan las tareas de manera concurrente
     await asyncio.gather(
             procesar_mensajes(client),
             bucle_control(client),
             tarea_destello()
         )
 
-
 try:
     asyncio.run(main())
 
 except KeyboardInterrupt:
-    r.value(1)
+    r.value(1) #se interrumpe el programa se apaga el rele 
 finally:
     asyncio.new_event_loop()
 
